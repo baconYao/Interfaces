0b7e1d3cfbc47fbd9d2428bb9261472e15310518
baconYao <bacon735392@gmail.com>
2020-01-14 20:19:51 +0800

    added the basic interface of bot

--- main.go
+++ main.go
@@ -4,6 +4,14 @@ import (
 	"fmt"
 )
 
+// 宣告一個 bot interface
+type bot interface {
+	// 如果你是一個擁有名 getGreeting function 且 return type 為 string的type，
+	// 那麼就有資格使用bot interface。
+	// 所以由於 englishBot&spanishBot 都有getGreeting method，因此可以使用bot interface
+	getGreeting() string
+}
+
 type englishBot struct {}
 type spanishBot struct {}
 
@@ -16,12 +24,8 @@ func main() {
 
 }
 
-func printGreeting(en englishBot) {
-	fmt.Println(en.getGreeting())
-}
-
-func printGreeting(sp spanishBot) {
-	fmt.Println(sp.getGreeting())
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
 func (en englishBot) getGreeting() string {
