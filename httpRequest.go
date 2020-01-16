3d1f2596cc6cd0413e848be21ddea5449941c47a
baconYao <bacon735392@gmail.com>
2020-01-17 01:14:47 +0800

    added anotation and used io.Copy to print the body of response

--- httpRequest.go
+++ httpRequest.go
@@ -4,14 +4,30 @@ import (
 	"os"
 	"net/http"
 	"fmt"
+	"io"
 )
 
 func getGooglePage() {
+	// https://golang.org/pkg/net/http/#Client.Get
 	resp, err := http.Get("http://google.com")
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	// 第一種印出 body 的方式
+	// make: 用來宣告一個有範圍限制的 slice
+	// 為何這裡要用 []byte? 因為 resp 是 *Response type  https://golang.org/pkg/net/http/#Response
+	// 從 resp 文件裡面找到 Body 會看到 Body io.ReadCloser，表示 Body 的 type 是 io.ReadCloser interface https://golang.org/pkg/io/#ReadCloser
+	// 從 ReadCloser interface 有 Reader 和 Closer 兩個 interface
+	// 最後在 Reader 這個 interface 看到 它定義的 method -> Read(p []byte) (n int, err error)
+	// 可以發現 Read 需要 []byte type 的參數。因此我們需要給予 []byte 到 resp.Body.Read()
+	bs := make([]byte, 99999)
+	resp.Body.Read(bs)
+	fmt.Println(string(bs))
+
+	fmt.Println("@@@@@@@@@@@@@@@@@@@@@@@")
+
+	// 第二種印出 body 的方式
+	io.Copy(os.Stdout, resp.Body)
 }
